642806cc9e392004365dd0a7e172b9b855db7f0c
Ilya Bruev <ilabruev77@gmail.com>
2025-05-05 23:15:18 +0300

    fixes

--- core/main/urls.py
+++ core/main/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('/about_us', views.about_us, name="about_us"),
-    path('/contacts', views.contacts, name="contacts"),
+    path('about_us', views.about_us, name="about_us"),
+    path('contacts', views.contacts, name="contacts"),
 ]
